3a95602085b2b462377dfa7939685dd43f369e2a
Frank Benitez Calzado <frank@MacBook-Pro-de-Frank.local>
2024-08-10 19:42:54 -0400

    pagination is done

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -8,7 +8,7 @@ import CaseByDate from '../../interfaces/casebydate';
 export class ApiService {
   #http = inject(HttpClient);   
 
-  #data = signal<CaseByDate[]>([]) ;
+  #data = signal<CaseByDate[]>([]); 
 
   constructor() { 
     this.#http.get<CaseByDate[]>('https://biostatistics.salud.pr.gov/cases/covid-19/grouped-by-earliest-positive-diagnostic-date')
@@ -21,5 +21,17 @@ export class ApiService {
 
   getDataSize(): number{
     return this.#data().length;
+  } 
+
+  setDateByDateRange(startDate:string,endDate:string){
+    let startDateFormated = startDate.replaceAll(':','%');
+    let endDateFormated = endDate.replaceAll(':','%');
+    //console.log(startDate);
+    //console.log(startDateFormated);
+    
+    this.#http.get<CaseByDate[]>(`https://biostatistics.salud.pr.gov/cases/covid-19/grouped-by-earliest-positive-diagnostic-date?StartDate=${startDateFormated}&EndDate=${endDateFormated}`)
+              .subscribe(res => this.#data.update(value => [...res]));
   }
+
+ 
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule,MatDatepickerInputEvent} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { ApiService } from '../services/api/api.service';
 import { CommonModule } from '@angular/common';
 
@@ -7,14 +11,60 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [MatProgressSpinner,CommonModule],
+  imports: [MatProgressSpinner,CommonModule,MatButtonModule,MatDatepickerModule,MatFormFieldModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 }) 
 export class TableComponent {
   public covidCases = inject(ApiService);
+  pageSize: number = 10;
+  currentPage = signal(1);
+  dataByPage = computed(()=>{
+    let startIndex = (this.currentPage() - 1) * this.pageSize;
+    return this.covidCases.getDataValue().slice(startIndex,startIndex + this.pageSize);
+  });
+
+  rangeDate = signal<Date[]>([]);
   
   constructor(){    
-    
+                
   }
+
+  next(){
+    this.currentPage.update(page => page + 1);
+  }
+
+  prev(){
+    this.currentPage.update(page => page - 1);
+  }
+
+  getDataByRangeDate(target:string, event: MatDatepickerInputEvent<Date>){     
+    if (event.value !== null) {
+      let value: Date = event.value as Date;
+      this.rangeDate.update(range => [...range,value]);
+    }        
+    if (this.rangeDate().length == 2) {
+      let diff = this.rangeDate()[1].getTime() - this.rangeDate()[0].getTime();     
+      if (diff < 0) {       
+        this.rangeDate.update(range => {
+          let result: Date[] = range;
+          result.shift();
+          return [...result];
+        });        
+      }else{
+        const week = 1000 * 60 * 60 * 24 * 7;
+        if (diff > week) {
+          alert("You can only select a week's worth of date at a time");
+        } else {
+          this.covidCases.setDateByDateRange(this.rangeDate()[0].toISOString(),this.rangeDate()[1].toISOString());
+        }          
+        
+        this.rangeDate.update(range => []); 
+      }     
+               
+    }  
+
+  }
+
 }
